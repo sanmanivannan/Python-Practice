2cbd960542187285c5e21473c045866165f2cebf
sanmanivannan <sanmanivannan@gmail.com>
2020-08-16 06:16:25 -0500

    reading and writing assignment

---
+++ Numberexe.py
@@ -0,0 +1,15 @@
+base = 15
+height = 22
+area = 1/2*(base*height)
+print(area)
+
+num_packets=9
+cost_per_packet=1.49
+total_cost=num_packets*cost_per_packet
+money_paid=20
+cash_back=money_paid-total_cost
+print(cash_back)
+
+length=5.5
+area=length**2
+print(area)
---
+++ variables.py
@@ -0,0 +1,9 @@
+birth_year=1983
+current_year=2020
+age=current_year - birth_year
+print(age)
+
+first="Tom"
+middle="Cruise"
+last="Mapother"
+print("Full Name:",first,middle,last)
