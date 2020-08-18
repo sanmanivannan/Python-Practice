ca46b17eceadcd42adf0b2755eda725a37920e55
sanmanivannan <sanmanivannan@gmail.com>
2020-08-18 01:07:21 -0500

    run file update

--- RUN.py
+++ RUN.py
@@ -20,3 +20,11 @@ print("List:",list)
 print("Tuple:",tuple)
 """
 
+"""program to accept a filename from the user and print the extension of that.
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+"""
+
+
+
