c296379eafd8f688ee96ce7f6e8f9a9dc163458c
sanmanivannan <sanmanivannan@gmail.com>
2020-08-18 01:04:22 -0500

    run file update

--- RUN.py
+++ RUN.py
@@ -1,13 +1,22 @@
-def findodd(num):
-    for i in range(len(num)):
-        for j in range(len(num)):
-            if i!=j:
-                product = num[i]*num[j]
-                if product & 1:
-                    return True
-                    return False
+"""import sys
+print(sys.version)
+print(sys.version_info)
 
+import datetime
+now = datetime.datetime.now
+print(now.strftime("%Y:%m:%d %H:%M:%S"))"""
 
+"""'''Write a Python program which accepts the radius of a circle from the user and compute the area.'''
+import math '''Or can be written as from math import pi'''
+r = float(input("enter the radius of the circle:"))
+area = math.pi*r*r
+print(area)"""
 
-num=[2,3,4,5,6]
-print(findodd(num))
+"""accepts a sequence of comma-separated numbers from user and generate a list and a tuple 
+nums = input("enter the list of number seperated by comma:")
+list=nums.split(",")
+tuple=tuple(list)
+print("List:",list)
+print("Tuple:",tuple)
+"""
+
