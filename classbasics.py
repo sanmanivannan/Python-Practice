055b726a82f816ceae7d058aaa825cb2e1153754
sanmanivannan <sanmanivannan@gmail.com>
2020-08-22 23:53:37 -0500

    run file update

---
+++ classbasics.py
@@ -0,0 +1,22 @@
+class Human:   # Creating a new class with a name Human
+    def __init__(self, n , o): # creating a first PROPERTY of the class Human
+        self.name = n
+        self.occupation = o
+
+    def do_work(self): # creating a first METHOD/FUNCTION under the class Human
+        if self.occupation == "Tennis":
+         print(self.name, "He/She is a Player")
+        elif self.occupation == "Acting":
+         print(self.name, "He/She is a Actor")
+
+    def speak(self):  # creating a second METHOD/FUNCTION under the class Human
+        print(self.name, " He/She can Speak")
+
+#Creating a OBJECT out of the Class Human
+tom = Human("Tom", "Acting")
+tom.do_work()
+tom.speak()
+
+sam = Human("Sam", "Tennis")
+sam.do_work()
+sam.speak()
