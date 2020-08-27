dec4ec59cd3fca32a08c52ca5f8eec0af84f72de
sanmanivannan <sanmanivannan@gmail.com>
2020-08-27 00:56:08 -0500

    run file update

--- SeleniumSessions/jquerydropdowns.py
+++ SeleniumSessions/jquerydropdowns.py
@@ -8,3 +8,26 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.implicitly_wait(5)
 driver.get("https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/")
 print(driver.title)
+
+driver.find_element(By.ID, 'justAnInputBox').click()
+time.sleep(5)
+dropdown_list = driver.find_elements(By.CSS_SELECTOR, 'span.comboTreeItemTitle')
+
+#simple logic for selecting the single option from the jquery dropdown
+"""for i in dropdown_list:
+    print(i.text)
+    if i.text == "choice 2 1":
+        i.click()
+        break"""
+
+#Write a function for the above logic to click an option from the jquery dropdown
+"""def select_options(droplist,value):
+    for j in droplist:
+        if j.text == value:
+            j.click()
+            break
+#provide the list and values as mentioned below
+#however, we cant always provide option selection values one by one if we have multiple selection, as its a crudew method
+select_options(dropdown_list,'choice 6')
+select_options(dropdown_list,'choice 2')
+select_options(dropdown_list,'choice 5')"""
