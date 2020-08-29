c516586cc5bf65d6934c590662c2550ca2dc486a
sanmanivannan <sanmanivannan@gmail.com>
2020-08-29 01:57:51 -0500

    run file update

---
+++ SeleniumSessions/Headless.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+#We use ChromeOptions method for HEADLESS OPERATIONS
+options = webdriver.ChromeOptions()
+options.headless = True
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+#In case if you are using without the webdriver manager also.,we can use the headless options
+#driver = webdriver.Chrome(executable_path="D:\\Learning\\Driver files\\chromedriver.exe", options=options)
+
+driver.get("https://www.amazon.in/")
+print(driver.title)
