7ea08629fdf7fcd39cd1f2d8a2fcd07102631aca
sanmanivannan <sanmanivannan@gmail.com>
2020-08-29 02:19:26 -0500

    run file update

--- SeleniumSessions/takeScreenshot.py
+++ SeleniumSessions/takeScreenshot.py
@@ -5,3 +5,11 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://www.amazon.in/")
+print(driver.title)
+driver.get_screenshot_as_file('1.png')
+
+#If we want to take the FULL ScreenSHOT of the page, we have to do it ONLY on the HEADLESS mode and use a LAMDBA function
+
+
