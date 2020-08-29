10bd71364d13513b47af6c8345d2748ab6aba003
sanmanivannan <sanmanivannan@gmail.com>
2020-08-29 18:14:51 -0500

    run file update

--- SeleniumSessions/DynamicWait.py
+++ SeleniumSessions/DynamicWait.py
@@ -23,9 +23,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
+#Using the WebDriverWait and expected conditions just to use the explicit wait for title and elements loading
+"""driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://app.hubspot.com/login')
+
 wait = WebDriverWait(driver,10)
 wait.until(ec.title_is('HubSpot Login'))
 print(driver.title)
@@ -33,5 +34,30 @@ print(driver.title)
 emailid = wait.until(ec.presence_of_element_located((By.ID, 'username')))
 emailid.send_keys('test@gmail.com')
 
-driver.find_element(By.ID,'password').send_keys('password')
+driver.find_element(By.ID,'password').send_keys('password')"""
+
+#Using WebDriverWait and expected_conditions to handle the ALERTS
+"""driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://mail.rediff.com/cgi-bin/login.cgi")
+
+wait = WebDriverWait(driver,10)
+
+driver.find_element(By.NAME, "proceed").click()
+alert = wait.until(ec.alert_is_present())
+print(alert.text)
+alert.accept()"""
+
+#Using WebDriverWait and expected_conditions to handle the LINKS
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://app.hubspot.com/login')
+
+wait = WebDriverWait(driver,10)
+
+signup = wait.until(ec.element_to_be_clickable((By.LINK_TEXT, 'Sign up')))
+signup.click()
+
+fname = wait.until(ec.presence_of_element_located((By.ID, 'uid-firstName-5')))
+fname.send_keys('fname')
+
+wait.until(ec.element_to_be_clickable((By.ID,"hs-eu-decline-button"))).click()
 
