a51eb675a767e26b9f62d43729c64b8b8e8b85ed
sanmanivannan <sanmanivannan@gmail.com>
2020-08-29 02:08:45 -0500

    run file update

---
+++ SeleniumSessions/takeScreenshot.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
