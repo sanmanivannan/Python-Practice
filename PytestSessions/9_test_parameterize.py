bec40a944a48223c981b6e3a34172ea2f670b3a2
sanmanivannan <sanmanivannan@gmail.com>
2020-09-04 02:00:33 -0500

    run file update

---
+++ PytestSessions/9_test_parameterize.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.mark.usefixtures('init__driver')
+class BaseTest:
+    pass
+
+class Test_Checker(BaseTest):
+    @pytest.mark.parametrize("username , password",
+                                [
+                                    ("uname1", "pword1"),
+                                    ("uname2", "pword2")
+                                ]
+                            )
+    def test_get_website(self, username, password):
+        self.driver.get("https://app.hubspot.com/login/")
+        self.driver.implicitly_wait(10)
+        self.driver.find_element(By.ID, 'username').send_keys(username)
+        self.driver.find_element(By.ID, 'password').send_keys(password)
